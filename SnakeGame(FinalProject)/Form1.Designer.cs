cfffecca491402fcdd7b2e9d7fa762122476ed8d
aaromarc327 <aaromarc327@SDS-L-763HC63>
2024-06-11 14:11:19 -0400

    Add project files.

---
+++ SnakeGame(FinalProject)/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace SnakeGame_FinalProject_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subtitleLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.imageLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 200;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(166, 443);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(269, 34);
+            this.titleLabel.TabIndex = 6;
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // subtitleLabel
+            // 
+            this.subtitleLabel.BackColor = System.Drawing.Color.Black;
+            this.subtitleLabel.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtitleLabel.ForeColor = System.Drawing.Color.White;
+            this.subtitleLabel.Location = new System.Drawing.Point(101, 477);
+            this.subtitleLabel.Name = "subtitleLabel";
+            this.subtitleLabel.Size = new System.Drawing.Size(399, 25);
+            this.subtitleLabel.TabIndex = 5;
+            this.subtitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLabel.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.ForeColor = System.Drawing.Color.White;
+            this.scoreLabel.Location = new System.Drawing.Point(101, 546);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(399, 25);
+            this.scoreLabel.TabIndex = 7;
+            this.scoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // imageLabel
+            // 
+            this.imageLabel.BackColor = System.Drawing.Color.Transparent;
+            this.imageLabel.ForeColor = System.Drawing.Color.Transparent;
+            this.imageLabel.Location = new System.Drawing.Point(110, 30);
+            this.imageLabel.Name = "imageLabel";
+            this.imageLabel.Size = new System.Drawing.Size(367, 413);
+            this.imageLabel.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(600, 600);
+            this.Controls.Add(this.imageLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.subtitleLabel);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Snake Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subtitleLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label imageLabel;
+    }
+}
+
---
+++ SnakeGame(FinalProject)/Form1.cs
@@ -0,0 +1,232 @@
+﻿//Aaron Marchanton
+//May 27, 2024
+//Snake Game
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace SnakeGame_FinalProject_
+{
+    public partial class Form1 : Form
+    {
+        Rectangle snake = new Rectangle(160, 300, 20, 20);
+        Rectangle apple = new Rectangle(405, 305, 10, 10);
+        SolidBrush redBrush = new SolidBrush(Color.DarkRed);
+        SolidBrush blueBrush = new SolidBrush(Color.CornflowerBlue);
+        SolidBrush orangeBrush = new SolidBrush(Color.Orange);
+
+        SoundPlayer eat = new SoundPlayer(Properties.Resources.eatSound);
+        SoundPlayer lose = new SoundPlayer(Properties.Resources.loseSound);
+
+        Random randValue = new Random();
+
+        int snakeSpeed = 20;
+        int snakeScore = 0;
+        int applep1 = 0;
+        int applep2 = 0;
+
+        string direction = "right";
+
+        List<Rectangle> bodyParts = new List<Rectangle>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (gameTimer.Enabled == false && snakeScore == 0)
+            {
+                titleLabel.Text = "Snake Game!";
+                subtitleLabel.Text = "Press Space to Start or Esc to End";
+                imageLabel.Image = Properties.Resources.snakeImage;
+            }
+            else if (gameTimer.Enabled == true)
+            {
+                imageLabel.Visible = false;
+
+                e.Graphics.FillEllipse(redBrush, snake);
+                e.Graphics.FillEllipse(blueBrush, apple);
+
+                for (int i = 0; i < bodyParts.Count(); i++)
+                {
+                    e.Graphics.FillEllipse(orangeBrush, bodyParts[i]);
+                }
+            }
+            else if (gameTimer.Enabled == false)
+            {
+                titleLabel.Text = "Snake Game!";
+                subtitleLabel.Text = "Press Space to Start or Esc to End";
+                scoreLabel.Text = $"Previous Score: {snakeScore}";
+
+                imageLabel.Visible = true;
+                imageLabel.Image = Properties.Resources.snakeImage;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            PlayerMovement();
+            CollisionWithWall();
+            CollisionWithSelf();
+            SnakeGetsApple();
+
+            Refresh();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    if (direction != "down")
+                    {
+                        direction = "up";
+                    }
+                    break;
+                case Keys.Down:
+                    if (direction != "up")
+                    {
+                        direction = "down";
+                    }
+                    break;
+                case Keys.Left:
+                    if (direction != "right")
+                    {
+                        direction = "left";
+                    }
+                    break;
+                case Keys.Right:
+                    if (direction != "left")
+                    {
+                        direction = "right";
+                    }
+                    break;
+                case Keys.Space:
+                    if (gameTimer.Enabled == false)
+                    {
+                        InitializeGame();
+                    }
+                    break;
+                case Keys.Escape:
+                    if (gameTimer.Enabled == false)
+                    {
+                        Application.Exit();
+                    }
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Space:
+                    gameTimer.Enabled = true;
+                    break;
+            }
+        }
+
+        public void PlayerMovement()
+        {
+            for (int i = bodyParts.Count - 1; i > 0; i--)
+            {
+                bodyParts[i] = bodyParts[i - 1];
+            }
+
+            bodyParts[0] = snake;
+
+            if (direction == "up")
+            {
+                snake.Y = snake.Y - snakeSpeed;
+            }
+            if (direction == "down")
+            {
+                snake.Y = snake.Y + snakeSpeed;
+            }
+            if (direction == "left")
+            {
+                snake.X = snake.X - snakeSpeed;
+            }
+            if (direction == "right")
+            {
+                snake.X = snake.X + snakeSpeed;
+            }
+        }
+
+        public void CollisionWithWall()
+        {
+            if (snake.Y > 580 || snake.Y < 0 || snake.X > 580 || snake.X < 0)
+            {
+                gameTimer.Enabled = false;
+
+                lose.Play();
+            }
+        }
+
+        public void CollisionWithSelf()
+        {
+            for (int i = 0; i < bodyParts.Count(); i++)
+            {
+                if (bodyParts[i].IntersectsWith(snake))
+                {
+                    gameTimer.Enabled = false;
+
+                    lose.Play();
+                }
+            }
+        }
+
+        public void SnakeGetsApple()
+        {
+            if (snake.IntersectsWith(apple))
+            {
+                snakeScore++;
+
+                eat.Play();
+
+                applep1 = randValue.Next(0, 30);
+                applep2 = randValue.Next(0, 30);
+
+                apple = new Rectangle(applep1 * 20 + 5, applep2 * 20 + 5, 10, 10);
+
+                bodyParts.Insert(bodyParts.Count, snake);
+            }
+        }
+
+        public void InitializeGame()
+        {
+            titleLabel.Text = "";
+            subtitleLabel.Text = "";
+            scoreLabel.Text = "";
+
+            gameTimer.Enabled = true;
+
+            snakeScore = 0;
+
+            direction = "right";
+
+            snake = new Rectangle(160, 300, 20, 20);
+            apple = new Rectangle(405, 305, 10, 10);
+
+            bodyParts.Clear();
+            bodyParts.Add(new Rectangle(140, 300, 20, 20));
+            bodyParts.Add(new Rectangle(120, 300, 20, 20));
+            bodyParts.Add(new Rectangle(100, 300, 20, 20));
+        }
+    }
+}
